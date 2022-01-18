5619496b71f0da0d2d87c42bdc8c50a213d10b12
Milind Mulinti <milindmulinti@gmail.com>
2022-01-18 11:02:18 +0530

    find_Kth_Largest

---
+++ src/arrrays/find_Kth_Largest.java
@@ -0,0 +1,16 @@
+package arrrays;
+
+import java.util.Arrays;
+
+public class find_Kth_Largest {
+    public static void main(String args[]) {
+        int[] a = {3,2,3,1,2,4,5,5,6};
+        int k = 4;
+        System.out.println(findKthLargest(a, k));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+
+        return 0;
+    }
+}
